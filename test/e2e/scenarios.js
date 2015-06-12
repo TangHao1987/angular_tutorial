05ccf210458b15d22876608614e6fdabb81c5e72
TangHao1987 <mythabc1987@gmail.com>
2015-06-12 14:18:45 +0800

    updated the test cases for step 10

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -72,8 +72,16 @@ describe('PhoneCat App', function () {
             browser.get('app/index.html#/phones/nexus-s');
         });
 
-        it('should display placeholder page with phoneId', function () {
-            expect(element(by.binding('phoneId')).getText()).toBe('nexus-s');
+        it('should display the first phone image as the main phone image', function() {
+            expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
+        });
+
+        it('should swap main image if a thumbnail image is clicked on', function() {
+            element(by.css('.phone-thumbs li:nth-child(3) img')).click();
+            expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.2.jpg/);
+
+            element(by.css('.phone-thumbs li:nth-child(1) img')).click();
+            expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
         });
     });
 });
